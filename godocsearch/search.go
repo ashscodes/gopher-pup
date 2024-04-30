c5eb1421adf03a4d862765eb0bb532b6833c2fbb
ashscodes <ash.sheehan@outlook.com>
2024-05-01 00:16:06 +0100

    Handle recursive and non-recursive search

--- godocsearch/search.go
+++ godocsearch/search.go
@@ -8,6 +8,13 @@ import (
 	"strings"
 )
 
+// SearchSettings contains the parsed CLI flag values.
+type SearchSettings struct {
+	Directory string
+	Keyword   string
+	Recursive bool
+}
+
 // containsKeyword checks if file contains the keyword value provided.
 func containsKeyword(filename, keyword string) bool {
 	file, err := os.Open(filename)
@@ -50,29 +57,37 @@ func printResults(results []string) {
 func searchFiles(settings SearchSettings) []string {
 	var results []string
 
-	// TODO: handle case when not recursive
 	walker := func(path string, fileInfo os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error accessing path '%s': %v\n", path, err)
 			return nil
 		}
 
-		if !fileInfo.IsDir() {
-			if containsKeyword(path, settings.Keyword) {
-				results = append(results, path)
+		if fileInfo.IsDir() {
+			if settings.Recursive {
+				if path != settings.Directory {
+					subDir := SearchSettings{Keyword: settings.Keyword, Recursive: settings.Recursive, Directory: path}
+					subDirResults := searchFiles(subDir)
+					results = append(results, subDirResults...)
+					return filepath.SkipDir
+				}
+			} else if path != settings.Directory {
+				return filepath.SkipDir
 			}
-		} else {
-			subDirSettings := SearchSettings{Keyword: settings.Keyword, Recursive: settings.Recursive, RootDir: path}
-			subDirResults := searchFiles(subDirSettings)
-			results = append(results, subDirResults...)
+
+			return nil
+		}
+
+		if containsKeyword(path, settings.Keyword) {
+			results = append(results, path)
 		}
 
 		return nil
 	}
 
-	err := filepath.Walk(settings.RootDir, walker)
+	err := filepath.Walk(settings.Directory, walker)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error walking directory '%s': %v\n", settings.RootDir, err)
+		fmt.Fprintf(os.Stderr, "Error walking directory '%s': %v\n", settings.Directory, err)
 	}
 
 	return results
